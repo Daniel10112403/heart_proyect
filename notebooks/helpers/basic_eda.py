2523106eaa37fbddf1f7a7531308598b91f78314
Daniel <daniel-arias11@hotmail.com>
2025-03-26 23:55:58 -0500

    created primarly eda funciton in helpers folder

---
+++ notebooks/helpers/basic_eda.py
@@ -0,0 +1,11 @@
+def eda(df, name, id='id'):
+    print('='*10)
+    print('.:STARTING EDA:.')
+    print('='*10)
+    print('DATA SET:', name)
+    print(df.head())
+
+    # Check general information
+#PENDING
+    # Check duplicates
+#PENDING
